c8ad30460f36e1b5ced4479363a0b4556bd47a9a
augustolzd <augustolzd@gmail.com>
2012-10-16 00:30:16 -0430

    Diseño y ajustes de interfaz

--- index.php
+++ index.php
@@ -2,20 +2,49 @@
 <html lang="Es_VE">
 <head>
 	<title> Proyecto </title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="lib/css/interfaz.css" type="text/css" />
+	<!--Para que funcione el CSS en varios navegaores-->
+	<script src="lib/js/prefixfree.min.js" type="text/javascript"></script>
+	<!--Funciones propias del sitio-->
+	<script src="lib/js/funciones.js" type="text/javascript"></script>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width = device-width, initial-scale=1, maximum-scale=1"/>
 </head>
 <body>
-<header>
-<section id="logo"><section>
-<section id="login"></section>	
-<nav></nav>
-</header>
+	<header>
+		<section id="logo">
+			<img src="lib/img/logo.png" id="imagen-logo"/>
+		</section>	
+		<nav id="menu-principal">
+			<ul>
+				<li class="link">Inicio</li>
+				<li class="link">Noticias</li>
+				<li class="link">Ventas</li>
+				<li class="link">Administración</li>
+				<li class="link">Solicitudes</li>
+			<ul>	
+		</nav>
+	</header>
+	<section id="cuerpo">
+		<section id="contenido">
+			<h1> Bienvenido al Portal del<br/> Consejo Comunal Centro Norte </h1>
 
-<form action="lib/agregar_noticia.php" >
-	<input type="text" placeholder="Autor" name="autor" required />
-	<input type="text" placeholder="Titulo" name="titulo" required />
-	<input type="text" placeholder="Noticia" name="noticia" required />
-	<input class="boton" type="submit" value="Cargar Noticia" />
-</form>	
+			<p class="text-normal">
+				Este portal tiene el proposito de informar a traves del medio digital sobre nuestros avances
+				en la comunidad, de igual manera para hacer llegar y atender las inquietudes de la comunidad.
+			</p>
 
+			<p class="text-normal">
+				Tendremos varias secciones para llevar acabo el intercambio de ideas o información, sección de noticias
+				disponible para informarse sobre nuestros logros como comunidad, libro de visitas en el cual podrán hacer
+				llegar sus quejas o inquietudes, además nuestra web se encargara de llevar el censo poblacional de la comunidad
+				como a la vez de generar las cartas de residencias de manera automatica, solo necesitando que este censado.
+			</p>	
+			<br />
+			<br />
+		</section>
+	<footer>Diseñado como proyecto socio tecnologico, Mision Sucre, Aldea Universitaria Gran Mariscal de Ayacucho</footer>	
+	</section>
 </body>
 <html>
